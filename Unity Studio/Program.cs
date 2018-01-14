8da4c4623faae8573179c382be3bee8467b1ad37
Perfare <hjyqwert@126.com>
2018-01-15 06:27:23 +0800

    update OpenTK to 3.0.0-pre

--- Unity Studio/Program.cs
+++ Unity Studio/Program.cs
@@ -13,7 +13,6 @@ namespace Unity_Studio
         [STAThread]
         static void Main()
         {
-            OpenTK.Toolkit.Init(new OpenTK.ToolkitOptions { EnableHighResolution = false });
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UnityStudioForm());
