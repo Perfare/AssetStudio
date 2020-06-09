1ffc07710da6ae0ef12f02211d20187be59eb5cf
George Wu <uiharu@buaa.edu.cn>
2020-06-09 23:53:09 +0200

    Deleted C++/CLI projects

--- Texture2DDecoder/astc.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASTC_H
-#define ASTC_H
-
-#include <stdint.h>
-
-int decode_astc(const uint8_t *, const long, const long, const int, const int, uint32_t *);
-
-#endif /* end of include guard: ASTC_H */
--- Texture2DDecoder/atc.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-int decode_atc_rgb4(const uint8_t* data, uint32_t m_width, uint32_t m_height, uint32_t* image);
-int decode_atc_rgba8(const uint8_t* data, uint32_t m_width, uint32_t m_height, uint32_t* image);
