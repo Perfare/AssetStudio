98314706318ee68dcc9a0d09b59721fc6495a2ba
George Wu <uiharu@buaa.edu.cn>
2020-06-13 03:25:14 +0200

    Handle DLL preloading on Linux and macOS

--- AssetStudio.PInvoke/DllLoader.cs
+++ AssetStudio.PInvoke/DllLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -11,13 +12,15 @@ namespace AssetStudio.PInvoke
         {
             var dllDir = GetDirectedDllDirectory();
 
+            // Not using OperatingSystem.Platform.
+            // See: https://www.mono-project.com/docs/faq/technical/#how-to-detect-the-execution-platform
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 Win32.LoadDll(dllDir, dllName);
             }
             else
             {
-                throw new NotImplementedException();
+                Posix.LoadDll(dllDir, dllName);
             }
         }
 
@@ -36,15 +39,21 @@ namespace AssetStudio.PInvoke
         private static class Win32
         {
 
-            internal static IntPtr LoadDll(string dllDir, string dllName)
+            internal static void LoadDll(string dllDir, string dllName)
             {
-                const string dllExtension = ".dll";
-                var directedDllPath = Path.Combine(dllDir, dllName + dllExtension);
+                var dllFileName = $"{dllName}.dll";
+                var directedDllPath = Path.Combine(dllDir, dllFileName);
 
                 // Specify SEARCH_DLL_LOAD_DIR to load dependent libraries located in the same platform-specific directory.
                 var hLibrary = LoadLibraryEx(directedDllPath, IntPtr.Zero, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
 
-                return hLibrary;
+                if (hLibrary == IntPtr.Zero)
+                {
+                    var errorCode = Marshal.GetLastWin32Error();
+                    var exception = new Win32Exception(errorCode);
+
+                    throw new DllNotFoundException(exception.Message, exception);
+                }
             }
 
             [DllImport("kernel32.dll", SetLastError = true)]
@@ -52,6 +61,52 @@ namespace AssetStudio.PInvoke
 
             private const uint LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x1000;
             private const uint LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x100;
+
+        }
+
+        private static class Posix
+        {
+
+            internal static void LoadDll(string dllDir, string dllName)
+            {
+                string dllExtension;
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    dllExtension = ".so";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    dllExtension = ".dylib";
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+
+                var dllFileName = $"lib{dllName}{dllExtension}";
+                var directedDllPath = Path.Combine(dllDir, dllFileName);
+
+                var hLibrary = DlOpen(directedDllPath, RTLD_NOW | RTLD_GLOBAL);
+
+                if (hLibrary == IntPtr.Zero)
+                {
+                    var pErrStr = DlError();
+                    var errorMessage = Marshal.PtrToStringAnsi(pErrStr);
+
+                    throw new DllNotFoundException(errorMessage);
+                }
+            }
+
+            [DllImport("libdl", EntryPoint = "dlopen")]
+            private static extern IntPtr DlOpen([MarshalAs(UnmanagedType.LPStr)] string fileName, int flags);
+
+            [DllImport("libdl", EntryPoint = "dlerror")]
+            private static extern IntPtr DlError();
+
+            private const int RTLD_LAZY = 0x1;
+            private const int RTLD_NOW = 0x2;
+            private const int RTLD_GLOBAL = 0x100;
 
         }
 
