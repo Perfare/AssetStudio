caa45216efa1028e78666b06ba548c4b41065374
Perfare <hjyqwert@126.com>
2021-05-28 22:16:50 +0800

    fix compilation error

--- AssetStudioFBXNative/asfbx_context.h
+++ AssetStudioFBXNative/asfbx_context.h
@@ -4,18 +4,6 @@
 #include <string>
 #include <unordered_set>
 
-namespace fbxsdk
-{
-	class FbxManager;
-	class FbxScene;
-	class FbxExporter;
-	template<typename T, const int Alignment = 16>
-	class FbxArray;
-	class FbxFileTexture;
-	class FbxSurfacePhong;
-	class FbxPose;
-}
-
 struct AsFbxContext
 {
 
