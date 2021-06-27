8dec09430402760c150cb2896460e570c24d5486
Perfare <hjyqwert@126.com>
2021-06-27 09:27:40 +0800

    update project files

--- AssetStudioGUI/Program.cs
+++ AssetStudioGUI/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,11 +9,14 @@ namespace AssetStudioGUI
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+#if !NETFRAMEWORK
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+#endif
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AssetStudioGUIForm());
